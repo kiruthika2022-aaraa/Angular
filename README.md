bc931575a3a86ffd44e940b64591f0d4038e079f
Kiruthika <kiruthika@dilligentech.com>
2025-12-24 12:37:54 +0530

    version 8.2.14

---
+++ README.md
@@ -0,0 +1,13 @@
+# Angular 8 UI Application
+
+## Requirements
+- Node.js 10.x
+- npm 6.x
+- Angular CLI 8
+
+## Run
+npm install
+ng serve
+
+## Build
+ng build
---
+++ src/app/app.component.ts
@@ -0,0 +1,2 @@
+import { Component } from '@angular/core';
+@Component({ selector: 'app-root', template: '<h1>Angular 8 UI App Works!</h1>' }) export class AppComponent {}
